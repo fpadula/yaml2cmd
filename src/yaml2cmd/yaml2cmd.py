0f5d1474b226d0ac64208e55f3a27789bc647a26
Felipe Padula Sanches <fpadula92@gmail.com>
2024-10-20 13:54:37 +1300

    Improve feedback when not specifying argument

--- src/yaml2cmd/yaml2cmd.py
+++ src/yaml2cmd/yaml2cmd.py
@@ -40,7 +40,8 @@ def main():
         formatter_class=ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument("-f", dest="yaml_file_path", help="Yaml file path", default="command.yaml")
+    parser.add_argument("-f", dest="yaml_file_path",
+                        help="Yaml file path", default="./command.yaml")
 
     parser.add_argument(
         "-v",
@@ -63,6 +64,11 @@ def main():
                 print(f"Could not process file '{yaml_file_path}'")
                 sys.exit(EX_NOINPUT)
     except FileNotFoundError:
+        if yaml_file_path == parser.get_default("yaml_file_path"):
+            print(
+                "Please specify a yaml file with the -f option or create a "
+                f"'{parser.get_default("yaml_file_path")}' file"
+            )
         print(f"Could not open file '{yaml_file_path}'")
         sys.exit(EX_NOINPUT)
 
